b1a8a6a4ef545ceb7b18d81748ef33fa9b36f14d
Milan Gotera <milangotera@gmail.com>
2022-09-14 16:01:28 -0400

    Agregando vista login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ *
+ * @fileoverview LoginPage
+ *
+ * @version 1.0
+ *
+ * @author Milan Gotera <milangotera@gmail.com>
+ *
+ */
+
+ import { Component, OnInit } from '@angular/core';
+
+ import { ApiService } from 'src/app/services/api/api.service';
+ import { HelperService } from 'src/app/services/helper/helper.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +20,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  form: any = {
+    email: null,
+    password: null,
+  };
+
+  errors: any = {
+    email: null,
+    password: null,
+  };
+
+  touch: any = {
+    email: false,
+    password: false,
+  };
+
+  load: boolean = false;
+
+  disabled: boolean = true;
+
+  show: boolean = false;
+
+  constructor(private api: ApiService, private helper: HelperService) { }
 
   ngOnInit() {
+  }
+
+  validate(input) {
+    this.disabled = true;
+
+    if (input == 'email') {
+      this.touch.email = true;
+      const emailValid = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+      if (emailValid.test(this.form.email)) {
+        this.errors.email = null;
+      } else {
+        this.errors.email = 'Ingresa un correo válido';
+      }
+    } else if (input == 'password') {
+      this.touch.password = true;
+      if (this.form.password == '' || this.form.password == null) {
+        this.errors.password = 'Ingresa una contraseña válida';
+      } else if (this.form.password.length < 6) {
+        this.errors.password = 'La contraseña debe tener al mínimo 6 caracteres';
+      } else {
+        this.errors.password = null;
+      }
+    }
+
+    if (!this.errors.email && !this.errors.password && this.touch.email && this.touch.password) {
+      this.disabled = false;
+    }
+  }
+
+  password() {
+    this.show = this.show ? false : true;
+  }
+
+  routes(route) {
+    this.helper.routes(route);
   }
 
 }
