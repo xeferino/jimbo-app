71cdb7f0c017ad7bda0b2135556a002e5b05a6ae
Milan Gotera <milangotera@gmail.com>
2022-09-14 17:16:50 -0400

    Agregando head

---
+++ src/app/components/head/head.component.ts
@@ -0,0 +1,27 @@
+/**
+ *
+ * @fileoverview HeadComponent
+ *
+ * @version 1.0
+ *
+ * @author Milan Gotera <milangotera@gmail.com>
+ *
+ */
+
+ import { Component, OnInit } from '@angular/core';
+
+ import { ApiService } from 'src/app/services/api/api.service';
+ import { HelperService } from 'src/app/services/helper/helper.service';
+
+@Component({
+  selector: 'app-head',
+  templateUrl: './head.component.html',
+  styleUrls: ['./head.component.scss'],
+})
+export class HeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -15,14 +15,17 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { TopComponent } from './top/top.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   imports: [CommonModule, IonicModule, FormsModule, ReactiveFormsModule],
   declarations: [
     TopComponent,
+    HeadComponent,
   ],
   exports: [
     TopComponent,
+    HeadComponent,
   ],
 })
 export class SharedModule {}
