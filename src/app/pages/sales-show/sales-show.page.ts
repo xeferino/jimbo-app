c37f42a94878d43d5f510d26101bce7c024c891f
Milan Gotera <milangotera@gmail.com>
2022-11-08 15:28:39 -0400

    vista de balance

--- src/app/pages/sales-show/sales-show.page.ts
+++ src/app/pages/sales-show/sales-show.page.ts
@@ -21,24 +21,9 @@ export class SalesShowPage implements OnInit {
 
   profile: any = JSON.parse(localStorage.getItem('profile'));
 
-  raffles: any = JSON.parse(localStorage.getItem('raffle'));
-
-  ticket: any = JSON.parse(localStorage.getItem('ticket'));
-
   load: boolean = true;
 
-  method: any = JSON.parse(localStorage.getItem('method')) || {
-    id: 0,
-    name: 'Pagar con mis JIB',
-    icon: 'assets/icon/jib.png',
-    type: 'jib'
-  };
-
-  methods: any = [];
-
-  cards: any = [];
-
-  sale: any = JSON.parse(localStorage.getItem('sale'));
+  shopping: any = JSON.parse(localStorage.getItem('sale'));
 
   constructor(private api: ApiService, private helper: HelperService) {}
 
@@ -49,11 +34,10 @@ export class SalesShowPage implements OnInit {
   loadData() {
     this.load = true;
     this.api
-      .get(`shoppings/tickets/${this.sale.id}`)
+      .get(`shoppings/tickets/${this.shopping.id}`)
       .then((response: any) => {
         this.load = false;
-        //this.methods = response.methods;
-        //this.cards   = response.cards;
+        this.shopping = response.shopping;
       })
       .catch((danger: any) => {
         this.load = false;
@@ -61,7 +45,7 @@ export class SalesShowPage implements OnInit {
   }
 
   back () {
-    this.routes(`profile/account`);
+    this.routes(`sales`);
   }
 
   routes(route){
