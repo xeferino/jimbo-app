a18cc1420d66aa2878aebeaee55545f21540b83a
Milan Gotera <milangotera@gmail.com>
2022-11-10 17:20:00 -0400

    Agregando vista de retiro

--- src/app/pages/retire/retire.page.ts
+++ src/app/pages/retire/retire.page.ts
@@ -42,51 +42,72 @@ export class RetirePage implements OnInit {
     type: 'card'
   };
 
+  min: number = 10;
+
+  max: number = this.profile.balance_usd;
+
+  usd: any = 0;
+
+  amount: any = 0;
+
+  change: any = this.profile.jib_rate;
+
+  account: any = null;
+
+  accounts: any = [];
+
+  account_load: boolean = false;
+
   constructor(private api: ApiService, private helper: HelperService, private modal: ModalController) {}
 
   ngOnInit() {
-    if(this.method.id != 0 && this.method.type == 'card') {
-      this.method = JSON.parse(localStorage.getItem('method'));
-      this.form.method_id = this.method.id;
-    } else {
-      this.method = {
-        id: 0,
-        name: `Escoge una tarjeta`,
-        icon: 'assets/icon/card.png',
-        type: 'card'
-      };
-    }
     localStorage.removeItem('jib');
-    //this.loadData();
+    this.loadData();
+    this.loadAccount();
   }
 
-  setAmout(item) {
-    this.form.amout_jib = item.jib;
-    this.form.amout_usd = item.usd;
+  setAmount($event) {
+    this.amount = $event.detail.value;
   }
 
-  setMethod() {
-    localStorage.setItem('route_method', 'recharge');
-    this.routes('methods');
+  setAccount(item) {
+    this.account = item;
   }
 
   loadData() {
     this.load = true;
     this.api
-      .get(`jibs`)
+    .get(`user/profile/${this.profile.id}`)
       .then((response: any) => {
-        this.load = false;
-        this.jibs = response.jibs;
+        this.load    = false;
+        this.profile = response.profile;
       })
       .catch((danger: any) => {
         this.load = false;
       });
   }
 
+  loadAccount() {
+    this.account_load = true;
+    this.api
+    .get(`user/accounts/all/${this.profile.id}`)
+      .then((response: any) => {
+        this.account_load    = false;
+        this.accounts = response.accounts;
+      })
+      .catch((danger: any) => {
+        this.account_load = false;
+      });
+  }
+
   buyData() {
     this.load = true;
     this.api
-      .post(`jibs/recharge`, this.form)
+      .post(`cash/request`, {
+        user_id: this.profile.id,
+        amount: this.amount,
+        account_user_id: this.account
+      })
       .then((response: any) => {
         this.load = false;
         this.helper.toast(response.message, 'Bien hecho');
@@ -103,8 +124,8 @@ export class RetirePage implements OnInit {
       component: ModalAlertPage,
       cssClass: 'app-modal modal-alert',
       componentProps: {
-        title: `Recargar ${this.form.amout_jib} JIBS`,
-        message: `¿Confirmas tu compra?`,
+        title: `Retirar ${this.amount} USD`,
+        message: `¿Confirmas tu retiro?`,
         icon: 'alert-outline',
         confirm: 'Confirmar',
         cancel: 'Cancelar'
