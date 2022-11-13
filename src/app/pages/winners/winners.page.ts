8bd2c2cac490003147953862726b4612a2af8e1d
Milan Gotera <milangotera@gmail.com>
2022-11-13 17:01:42 -0400

    Agregando vista de ganadores

--- src/app/pages/winners/winners.page.ts
+++ src/app/pages/winners/winners.page.ts
@@ -45,48 +45,7 @@ export class WinnersPage implements OnInit {
       .get(`raffles/winners`)
       .then((response: any) => {
         this.load = false;
-        //this.winners = response.raffles;
-
-        this.winners = [
-            {
-                "id": 1,
-                "title": "Ganate 1000$",
-                "cash_to_draw": "$1,000.00",
-                "date_start": "2022-11-11T00:00:00.000000Z",
-                "date_end": "2022-11-14T00:00:00.000000Z",
-                "date_release": "2022-10-12T00:00:00.000000Z",
-                "days_ago": 32,
-                "logo": "http://jimbo.test/assets/images/raffles/raffle.jpg",
-                "winners": [
-                    {
-                        "id": 1,
-                        "name": "Jose Lozada",
-                        "dni": "20373816",
-                        "phone": "41425365897",
-                        "email": "jose_gle@gmail.com",
-                        "address": "Cariaco",
-                        "country_id": 1,
-                        "amount": "$1,000.00",
-                        "ticket_id_parent": "16361c5514b7441",
-                        "ticket_id_winner": "111667352347",
-                        "seller_id": 4,
-                        "user_id": null,
-                        "raffle_id": 1,
-                        "created_at": "2022-11-13T13:54:38.000000Z",
-                        "image": "http://jimbo.test/assets/images/avatar.svg",
-                        "country": {
-                            "id": 1,
-                            "name": "Peru",
-                            "iso": "PE",
-                            "code": "+51",
-                            "icon": "http://jimbo.test/assets/images/flags/1668296507.jpg"
-                        }
-                    }
-                ]
-            }
-        ];
-
-        console.log(this.winners.lenght)
+        this.winners = response.raffles;
       })
       .catch((danger: any) => {
         this.load = false;
